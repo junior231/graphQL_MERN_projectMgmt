515e06eff1d028d5adca03eb6ad039a76ba597f8
junior231 <ilocollins231@gmail.com>
2023-01-18 22:03:18 -0500

    Add heroku-postbuild script

--- server/index.js
+++ server/index.js
@@ -26,4 +26,17 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static folder
+  app.use(express.static(path.join(__dirname, '../client/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Project...' })
+  })
+}
+
 app.listen(port, console.log(`Server running on port ${port}`));
